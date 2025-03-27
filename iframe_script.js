8eaa44784ec51cfa17b23dd1715a34cf6b58e4ae
Dseekeroftruth <dseekeroftruth@gmail.com>
2025-03-27 04:15:44 -0700

    Delete iframe_script.js

--- iframe_script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const isInIframe = window.self !== window.top; // Detect if inside iframe
-
-    if (isInIframe) {
-        document.body.classList.add("iframe-mode"); // Apply iframe-specific styles
-    }
-});
